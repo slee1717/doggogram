8fbde3ebdd5474a6733bcdb7cc070b9385a9bbcc
slee1717 <samx17@gmail.com>
2023-04-04 22:25:44 -0400

    added update states

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './EditPost.css'
 import { supabase } from '../client'; // assuming you have imported and configured supabaseClient
 
-const EditPost = ({data}) => {
+const EditPost = ({ data }) => {
 
-    const {id} = useParams();
-    const post = data.filter(item => item.id === id)[0];
+    const { id } = useParams();
+    const [post, setPost] = useState({ title: '', author: '', description: '' });
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const { data: postFromDB, error } = await supabase
+                .from('Posts')
+                .select('*')
+                .eq('id', id)
+                .single();
+
+            if (error) {
+                console.error('Error fetching post:', error);
+            } else {
+                setPost(postFromDB);
+            }
+        };
+
+        fetchPost();
+    }, [id]);
 
     const deletePost = async (event) => {
         event.preventDefault();
@@ -19,22 +37,38 @@ const EditPost = ({data}) => {
         window.location = "http://localhost:3000/";
     }
 
+    const updatePost = async (event) => {
+        event.preventDefault();
+
+        await supabase
+        .from('Posts')
+        .update({ title: post.title, author: post.author,  description: post.description})
+        .eq('id', id);
+
+        window.location = "/";
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setPost(prevPost => ({ ...prevPost, [name]: value }));
+    }
+
     return (
         <div>
             <form>
-                <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" value={post.title} /><br />
-                <br/>
+                <label htmlFor="title">Title</label> <br />
+                <input type="text" id="title" name="title" value={post.title} onChange={handleChange} /><br />
+                <br />
 
-                <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" value={post.author} /><br />
-                <br/>
+                <label htmlFor="author">Author</label><br />
+                <input type="text" id="author" name="author" value={post.author} onChange={handleChange} /><br />
+                <br />
 
-                <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" value={post.description} >
+                <label htmlFor="description">Description</label><br />
+                <textarea rows="5" cols="50" id="description" name="description" value={post.description} onChange={handleChange} >
                 </textarea>
-                <br/>
-                <input type="submit" value="Submit" />
+                <br />
+                <input type="submit" value="Submit" onClick={updatePost}/>
                 <button className="deleteButton" onClick={deletePost}>Delete</button>
             </form>
         </div>
